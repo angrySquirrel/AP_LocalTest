05e7b6f3df133ba1447b4d261da187028c8ee978
squirrel <1043030172@qq.com>
2016-01-16 21:20:04 +0800

    add NMI evalutaion
    add NMI evalutaion

--- test1/getSimilarityMatrix.py
+++ test1/getSimilarityMatrix.py
@@ -8,26 +8,30 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 import shutil
+import platform
 
 from scipy.stats.stats import itemfreq
 #from skimage.feature import local_binary_pattern
 from sklearn.cluster import AffinityPropagation
 import numpy as np
 from numpy import mean
-
+from sklearn.metrics.cluster import normalized_mutual_info_score
 #获得文件夹下所有的图像的路径
 def get_Imlist(path):  
         paths = []
-        files = []
+        classId = []
+#         files = []
         for f in os.listdir(path): 
             if f.endswith('.jpg') :    
                 paths.append(os.path.join(path,f))
-                files.append(int(f.split('_')[-1].split('.')[0]))
+                # input the image Name, get the label idx, eg, 001_0001.jpg means the label idx is 1
+                classId.append(int(f.split('_')[0])) # convert string to integer  
+#                 files.append(int(f.split('_')[-1].split('.')[0]))
 #         files,paths = zip(*sorted(zip(files, paths)))
-        return paths
+        return paths,classId
 #获得全部图像特征
 def get_Feature(imgFolder):     
-        imList = get_Imlist(imgFolder)
+        imList,classId = get_Imlist(imgFolder)
         counter = 0
         imgDes=[] 
         sift = cv2.SIFT()  # @UndefinedVariable
@@ -43,9 +47,9 @@ def get_Feature(imgFolder):
                 print des.shape
                 imgDes.append(des)
                 # draw the keypoints
-                img_show = cv2.drawKeypoints(img_gray,kp)
-                cv2.imwrite('sift'+str(counter)+'.jpg',img_show)
-        return imgDes,counter,imList
+#                 img_show = cv2.drawKeypoints(img_gray,kp)
+#                 cv2.imwrite('sift'+str(counter)+'.jpg',img_show)
+        return imgDes,counter,imList,classId
 
 #遍历文件夹生成相似度矩阵
 def  get_SimilairtyMatrix(imgDes,num):
@@ -73,75 +77,88 @@ def  get_SimilairtyMatrix(imgDes,num):
         return  similarity
 
 
+    
 
-# imgFolder = '/home/hadoop/桌面/132/small'
-imgFolder = 'C:\Users\Administrator\Desktop\dataset'
-pathImages = imgFolder
+if __name__ == '__main__':
+ 
+    # imgFolder = '/home/hadoop/桌面/132/small'
+#     if platform.system() == "Windows" :
+#         
+#     else:
+    
+    imgFolder = 'C:\Users\Administrator\Desktop\dataset'
+    pathImages = imgFolder
+    
+    # delete existed folders
+    
+    if os.path.exists(os.path.join(imgFolder,'CenterImages')):
+        shutil.rmtree(os.path.join(imgFolder,'CenterImages'))
+    if os.path.exists(os.path.join(imgFolder,'Clusters')):
+        shutil.rmtree(os.path.join(imgFolder,'Clusters'))
+    
+    # pathImages = imgFolder+'/img2'
+    imgDes,numOfImgs,imPaths,classId=get_Feature(pathImages)
+    S=get_SimilairtyMatrix(imgDes,numOfImgs)
+#     pre = S.min()
+    pre = np.median(np.median(S, axis = 0))
+    print 'the median value is ' + str(pre)
+    af = AffinityPropagation(preference=pre, verbose=True,max_iter=500,affinity="precomputed").fit(S)
+    labels = af.labels_
+    nmiScore = normalized_mutual_info_score(classId,labels)
+    print 'classId' + ''.join([str(e) for e in classId ,' '])
+    print 'clustering result' + ''.join([str(e) for e in labels,' '])
+    print 'the NMI Score of classification is:' + str(nmiScore)
+    cluster_centers_indices = af.cluster_centers_indices_
+    n_clusters_ = len(cluster_centers_indices)
+    #################################################################
+    #
+    # Create folder with central images for each cluster
+    #
+    #################################################################  
+          
+    
+    #obtain representative images for each cluster
+    central_ims =  cluster_centers_indices
+    
+    central_folder = os.path.join(imgFolder,'CenterImages')
+    if not os.path.exists(central_folder):
+            os.makedirs(central_folder)    
+    
+    count=0
+    for central_im in central_ims:
+            filename = os.path.join(central_folder,'Cluster_'+str(count)+'.jpg')
+            img = cv2.imread(imPaths[central_im],1)
+            cv2.imwrite(filename, img)             
+            count = count + 1
+             
+    #ADDED
+    #################################################################
+    #
+    # Separate Clusters into folders
+    #
+    #################################################################           
+       
+    clusters_folder = os.path.join(imgFolder,'Clusters')
+    if not os.path.exists(clusters_folder):
+            os.makedirs(clusters_folder) 
+    clust_dir = []
+    print 'number of clusters : ' + str(n_clusters_ )
+    for iclust in range(0,n_clusters_ ):
+            direc = os.path.join(clusters_folder,'Cluster_'+str(iclust))
+            if not os.path.exists(direc):
+                    os.makedirs(direc)         
+            clust_dir.append(direc)
+          
+    for im in range(0,len(imPaths)):
+    #         im_name = imPaths[im].split('/')[-1] # for linux
+            im_name = imPaths[im].split('\\')[-1]
+            print clust_dir[int(labels[im])]
+            filename = os.path.join(clust_dir[int(labels[im])],im_name)
+            #print filename
+            img = cv2.imread(imPaths[im],1)
+            cv2.imwrite(filename, img)          
 
-# delete existed folders
-
-if os.path.exists(os.path.join(imgFolder,'CenterImages')):
-    shutil.rmtree(os.path.join(imgFolder,'CenterImages'))
-if os.path.exists(os.path.join(imgFolder,'Clusters')):
-    shutil.rmtree(os.path.join(imgFolder,'Clusters'))
-
-# pathImages = imgFolder+'/img2'
-imgDes,numOfImgs,imPaths=get_Feature(pathImages)
-S=get_SimilairtyMatrix(imgDes,numOfImgs)
-pre = S.min()
-af = AffinityPropagation(preference=pre, verbose=True,max_iter=500,affinity="precomputed").fit(S)
-labels = af.labels_
-cluster_centers_indices = af.cluster_centers_indices_
-n_clusters_ = len(cluster_centers_indices)
-#################################################################
-#
-# Create folder with central images for each cluster
-#
-#################################################################  
-      
-
-#obtain representative images for each cluster
-central_ims =  cluster_centers_indices
-
-central_folder = os.path.join(imgFolder,'CenterImages')
-if not os.path.exists(central_folder):
-        os.makedirs(central_folder)    
-
-count=0
-for central_im in central_ims:
-        filename = os.path.join(central_folder,'Cluster_'+str(count)+'.jpg')
-        img = cv2.imread(imPaths[central_im],1)
-        cv2.imwrite(filename, img)             
-        count = count + 1
-         
-#ADDED
-#################################################################
-#
-# Separate Clusters into folders
-#
-#################################################################           
-   
-clusters_folder = os.path.join(imgFolder,'Clusters')
-if not os.path.exists(clusters_folder):
-        os.makedirs(clusters_folder) 
-clust_dir = []
-print 'number of clusters : ' + str(n_clusters_ )
-for iclust in range(0,n_clusters_ ):
-        direc = os.path.join(clusters_folder,'Cluster_'+str(iclust))
-        if not os.path.exists(direc):
-                os.makedirs(direc)         
-        clust_dir.append(direc)
-      
-for im in range(0,len(imPaths)):
-#         im_name = imPaths[im].split('/')[-1] # for linux
-        im_name = imPaths[im].split('\\')[-1]
-        print clust_dir[int(labels[im])]
-        filename = os.path.join(clust_dir[int(labels[im])],im_name)
-        #print filename
-        img = cv2.imread(imPaths[im],1)
-        cv2.imwrite(filename, img)          
-
-
+    
    
                 
                 
